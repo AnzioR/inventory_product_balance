eea02b2b2ad73a1ea334f7f5570fb196889bd952
AnzioR <zkxmtpdl@gmail.com>
2023-05-02 15:25:46 +0900

    setting

---
+++ ipb/src/main/java/com/ipb/IpbApplication.java
@@ -0,0 +1,13 @@
+package com.ipb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpbApplication.class, args);
+	}
+
+}
---
+++ ipb/src/test/java/com/ipb/IpbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
