6af0000c6d187ea83a455ea2575594af8d6af2b3
채란 <122114197+luckyKite@users.noreply.github.com>
2023-05-03 16:06:00 +0900

    store, ã event, eventType

---
+++ ipb/src/main/java/com/ipb/service/EventTypeService.java
@@ -0,0 +1,41 @@
+package com.ipb.service;
+
+import com.ipb.domain.EventType;
+import com.ipb.frame.MyService;
+import com.ipb.mapper.EventTypeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventTypeService implements MyService<Long, EventType> {
+
+  @Autowired
+  EventTypeMapper eventTypeMapper;
+
+  @Override
+  public void register(EventType eventType) throws Exception {
+    eventTypeMapper.insert(eventType);
+  }
+
+  @Override
+  public void modify(EventType eventType) throws Exception {
+    eventTypeMapper.update(eventType);
+  }
+
+  @Override
+  public void remove(Long id) throws Exception {
+    eventTypeMapper.delete(id);
+  }
+
+  @Override
+  public EventType get(Long id) throws Exception {
+    return eventTypeMapper.select(id);
+  }
+
+  @Override
+  public List<EventType> get() throws Exception {
+    return eventTypeMapper.selectall();
+  }
+}
