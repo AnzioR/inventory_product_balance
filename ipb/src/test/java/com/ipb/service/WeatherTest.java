7cd5e503c7345ea4c5478c12123af931cef44d3f
HeoJoonHo1 <121837814+HeoJoonHo1@users.noreply.github.com>
2023-05-05 23:11:41 +0900

    OpenWeather

--- ipb/src/test/java/com/ipb/service/WeatherTest.java
+++ ipb/src/test/java/com/ipb/service/WeatherTest.java
@@ -1,6 +1,10 @@
 package com.ipb.service;
 
+import com.ipb.utill.OpenWeatherUtill;
 import com.ipb.utill.WeatherUtill;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,5 +33,28 @@ public class WeatherTest {
     Date date = new Date();
     SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
     System.out.println(sdf);
+  }
+  @Test
+  public void weateher() throws Exception {
+    String busan = OpenWeatherUtill.getWeather("busan");
+    JSONParser jsonparser = new JSONParser();
+    JSONObject weather = (JSONObject)jsonparser.parse(busan);
+    JSONArray weatherA = (JSONArray) weather.get("weather");
+    JSONObject WeatherB = (JSONObject) weatherA.get(0);
+    String EWeather = (String)WeatherB.get("main");
+    String KWeather = (String)WeatherB.get("description");
+
+
+
+    JSONObject weatherC = (JSONObject) weather.get("main");
+    Double temps = (Double) weatherC.get("temp");
+
+
+    System.out.println("(영어)날씨 = " + EWeather);
+    System.out.println("(한국)날씨 = " + KWeather);
+    System.out.println("기온 = " + temps);
+
+
+
   }
 }
