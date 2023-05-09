607c385195ef674fbd5464d62af7d4e128c6a711
HeoJoonHo1 <121837814+HeoJoonHo1@users.noreply.github.com>
2023-05-09 21:19:55 +0900

    weather rework

--- ipb/src/main/java/com/ipb/controller/StaffController.java
+++ ipb/src/main/java/com/ipb/controller/StaffController.java
@@ -52,6 +52,7 @@ public class StaffController {
         //DB에 날씨 데이터 최종 저장하기
         weatherService.register(new_weather);
         return loginStaff;
+        //원래 대로라면 날씨랑 같이 넣어줘야해
     }
 
     @PostMapping("/weather")
@@ -60,7 +61,7 @@ public class StaffController {
         // 이건 3시간 단위로 조회할꺼야
 
         // 기존 날씨 데이터 조회
-        Weather old_weather = weatherService.get(staff.getStore_id()); // SELECT * FROM ipb1.weather where store_id = 3 order by weather_date desc limit 1;
+//        Weather old_weather = weatherService.get(staff.getStore_id()); // SELECT * FROM ipb1.weather where store_id = 3 order by weather_date desc limit 1;
 
         // 새로운 날씨 데이터
         String weather_json = OpenWeatherUtill.getWeather(staff.getArea());
@@ -69,16 +70,16 @@ public class StaffController {
         new_weather.setStore_id(staff.getStore_id());
         weatherService.register(new_weather);
 
-        boolean isSame = old_weather.getStatus().equals(new_weather.getStatus());
-        if(isSame) {
-            // 날씨가 기존의 날씨와 동일하면 false 반환
-            return new WeatherStatus(false, null);
-//                    return "{\"바꿨냐\": false}";
-        } else {
+//        boolean isSame = old_weather.getStatus().equals(new_weather.getStatus());
+//        if(isSame) {
+//            // 날씨가 기존의 날씨와 동일하면 false 반환
+//            return new WeatherStatus(false, null);
+////                    return "{\"바꿨냐\": false}";
+//        } else {
             // 날씨가 기존의 날씨와 달라지면 true 및 바뀐 날씨 반환
             return new WeatherStatus(true, new_weather.getStatus());
 //                    return "{\"바꿨냐\": true, \"바뀐날씨\":" + new_weather.getStatus() +"}";
-        }
+//        }
     }
 
 
