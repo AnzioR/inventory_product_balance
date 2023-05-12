46803dfd715010a17e44f05f80677053a5f5703e
윤민지 <yummmyg1@gmail.com>
2023-05-12 09:38:34 +0900

    MODI.orderissue,salesissue

---
+++ ipb/src/main/java/com/ipb/service/SalesIssueService.java
@@ -0,0 +1,48 @@
+package com.ipb.service;
+
+import com.ipb.domain.OrdersIssue;
+import com.ipb.domain.SalesIssue;
+import com.ipb.frame.MyService;
+import com.ipb.mapper.OrdersIssueMapper;
+import com.ipb.mapper.SalesIssueMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SalesIssueService implements MyService<Long, SalesIssue> {
+
+  @Autowired
+  SalesIssueMapper salesIssueMapper;
+
+
+  @Override
+  public void register(SalesIssue salesIssue) throws Exception {
+    salesIssueMapper.insert(salesIssue);
+  }
+
+  @Override
+  public void modify(SalesIssue salesIssue) throws Exception {
+    salesIssueMapper.update(salesIssue);
+  }
+
+  @Override
+  public void remove(Long id) throws Exception {
+    salesIssueMapper.delete(id);
+  }
+
+  @Override
+  public SalesIssue get(Long id) throws Exception {
+    return salesIssueMapper.select(id);
+  }
+
+  @Override
+  public List<SalesIssue> get() throws Exception {
+    return salesIssueMapper.selectall();
+  }
+
+//  public List<SalesIssue> ordersissueproductlist(Long store_id) throws Exception {
+//    return salesIssueMapper.ordersissueproductlist(store_id);
+//  }
+}
