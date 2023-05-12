f056f998f626b72ccf535ce65c9f0a3feae5f7c9
채란 <122114197+luckyKite@users.noreply.github.com>
2023-05-12 10:07:00 +0900

    event modify...sorry

--- ipb/src/main/java/com/ipb/mapper/EventMapper.java
+++ ipb/src/main/java/com/ipb/mapper/EventMapper.java
@@ -5,11 +5,9 @@ import com.ipb.frame.MyMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Mapper
 @Repository
-public interface EventMappper extends MyMapper<Long, Event> {
+public interface EventMapper extends MyMapper<Long, Event> {
   public Event searcheventname(String name) throws Exception;
 
   public Event selectbytype(Long id) throws Exception;
