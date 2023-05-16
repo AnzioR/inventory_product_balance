fa00ade57733606618743ed92b5249ce4890b658
AnzioR <zkxmtpdl@gmail.com>
2023-05-16 09:26:49 +0900

    requestbody added

--- ipb/src/main/java/com/ipb/controller/StoreController.java
+++ ipb/src/main/java/com/ipb/controller/StoreController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -29,7 +30,7 @@ public class StoreController {
 
   //점포등록
   @PostMapping("/store/add")
-  public Store register(Store store){
+  public Store register(@RequestBody Store store){
     try {
       storeService.register(store);
       return store;
