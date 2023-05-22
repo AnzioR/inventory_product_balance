f969c80d437854732819d853a129fbc138612652
HeoJoonHo1 <121837814+HeoJoonHo1@users.noreply.github.com>
2023-05-22 18:01:29 +0900

    auto event_product

--- ipb/src/main/java/com/ipb/mapper/EventProductMapper.java
+++ ipb/src/main/java/com/ipb/mapper/EventProductMapper.java
@@ -11,5 +11,7 @@ import java.util.List;
 @Repository
 public interface EventProductMapper extends MyMapper<Long, EventProduct> {
   public List<EventProduct> searcheventproduct(String name) throws Exception;
+  public List<EventProduct> searcheventproductbystartdate(String start_date) throws Exception;
+
 }
 
