e088c84431b87e9414258a2434c5f4ca8a29606e
HeoJoonHo1 <121837814+HeoJoonHo1@users.noreply.github.com>
2023-05-25 16:28:45 +0900

    auto event

--- ipb/src/main/java/com/ipb/domain/EventAutoOrders.java
+++ ipb/src/main/java/com/ipb/domain/EventAutoOrders.java
@@ -17,4 +17,12 @@ public class EventAutoOrders {
     private Long store_id;
     private String product_name;
     private Long product_id;
+    private Long product_info_id;
+    private Integer cost;
+    private Integer price;
+    private String name;
+
+    public EventAutoOrders(Long event_product_id) {
+        this.event_product_id = event_product_id;
+    }
 }
